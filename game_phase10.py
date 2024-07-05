6dd547853499a5b55e74bb1ced6ec8d41247e6d3
Steven Kovarik <133057101+Kov1331@users.noreply.github.com>
2024-07-05 16:32:14 -0400

    Many Changes

--- game_phase10.py
+++ game_phase10.py
@@ -5,10 +5,19 @@ Builder.load_file("game_phase10.kv")
 
 
 class Phase10(Screen):
-    ...
 
     def add_player(self):
         ...
 
     def check_button(self):
+        if self.text == '':
+            self.text = 'X'
+        else:
+            self.text = '' # todo fix this error?
+
+    def reset_game(self):
         ...
+
+    def add_score(self):
+        score_0 = self.text
+        print(score_0)
